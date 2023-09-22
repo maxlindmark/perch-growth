18044228679ae17ff574ea3d038c0544688f15cc
Max Lindmark <max.lindmark@slu.se>
2023-09-22 08:33:03 +0200

    update figures

--- R/map-sample-plot.R
+++ R/map-sample-plot.R
@@ -34,22 +34,19 @@ utm_zone33 <- 32634
 swe_coast_proj <- sf::st_transform(swe_coast, crs = utm_zone33)
 
 # Add points and use same color palette as in VBGE and temp plot
-# Order for plotting colors (but not facet order) (see temperature fitting script!)
-order <- data.frame(area = c("SI_HA", "TH", "BS", "BT", "FM", "SI_EK", "MU", "FB", "HO", "JM", "RA")) 
-
-nareas <- length(unique(order$area)) + 2 # to skip the brightest colors
-colors <- colorRampPalette(brewer.pal(name = "RdYlBu", n = 10))(nareas)[-c(7,8)]
+# Order for plotting colors (but not facet order) (see vbge fitting script!)
+order <- read_csv(paste0(home, "/output/ranked_temps.csv"))
 
 # Read for sample size per area
-vbg <- read_csv(paste0(home, "/output/vbg.csv")) |> filter(!area == "VN")
+vbg <- read_csv(paste0(home, "/output/vbg.csv"))
 
 df <- data.frame(area = c("BS", "BT", "FB", "FM", "HO", "JM", "MU", "RA",
-                          "SI_EK", "SI_HA", "TH"),
+                          "SI_EK", "SI_HA"),
                  area_name = c("Brunskär", "Biotest*", "Finbo", "Forsmark",
                                "Holmön", "Kvädofjärden", "Muskö", "Rånea",
-                               "Simpevarp Ek", "Simpevarp Ha*", "Torhamn"),
-                 lon = c(21.5, 18.1, 19.5, 18, 20.9, 16.8, 18.1, 22.3, 16.6, 16.7, 15.9),
-                 lat = c(60, 60.4, 60.3, 60.5, 63.7, 58, 59, 65.9, 57.3, 57.4, 56.1))
+                               "Simpevarp Ek", "Simpevarp Ha*"),
+                 lon = c(21.5, 18.1, 19.5, 18, 20.9, 16.8, 18.1, 22.3, 16.6, 16.7),
+                 lat = c(60, 60.4, 60.3, 60.5, 63.7, 58, 59, 65.9, 57.3, 57.4))
 
 df <- add_utm_columns(df, ll_names = c("lon", "lat"), units = "m")
 
@@ -70,7 +67,7 @@ p1 <-
   labs(x = "Longitude", y = "Latitude") +
   xlim(xmin, xmax) +
   ylim(ymin, ymax) +
-  annotate("text", label = "Sweden", x = xmin + 0.23*xrange, y = ymin + 0.6*yrange,
+  annotate("text", label = "SWEDEN", x = xmin + 0.23*xrange, y = ymin + 0.6*yrange,
            color = "grey30", size = 4) +
   geom_point(data = df, aes(X, Y, fill = factor(area_name, order$area_name)), size = 3, inherit.aes = FALSE,
              shape = 21, color = "white") +
@@ -103,7 +100,7 @@ p2 <- ggplot(vbg, aes(cohort, k_median, size = n, color = factor(area_full, leve
   facet_wrap(~factor(area_full, levels = order_facet$area_full), ncol = 2) + 
   scale_color_manual(values = colors, name = "Area") +
   scale_fill_manual(values = colors, name = "Area") +
-  theme(legend.position = c(0.65, 0.07),
+  theme(legend.position = c(0.11, 0.96),
         legend.key.height = unit(0.01, 'cm'), 
         legend.text = element_text(size = 6),
         legend.title = element_text(size = 7),
